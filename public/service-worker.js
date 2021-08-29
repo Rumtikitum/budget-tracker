ed73383aa0cae36dce5453727a2c3b134b80cebf
Richie Tauch <rtauc001@gmail.com>
2021-08-29 13:33:09 -0700

    building out service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,4 +22,20 @@ const FILES_TO_CACHE = [
   
     self.skipWaiting();
   });
+
+
+  self.addEventListener("activate", (evt) => {
+    evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
   
+    self.clients.claim();
+  });
